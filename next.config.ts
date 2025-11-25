44f41c1c209384c6209c85b9453620d9a6798c3d
carlos-burelo <carlosfernandoburelo@gmail.com>
2025-11-25 15:18:34 -0600

    :bug: minor changes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	output: "standalone",
+	  basePath: "",
+  assetPrefix: "",
 	experimental: {
 		serverActions: {
 			bodySizeLimit: "50mb",
