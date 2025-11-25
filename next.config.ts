d182c73c06a036582a61bc7e6f6f14b11f1d04fe
carlos-burelo <carlosfernandoburelo@gmail.com>
2025-11-25 15:24:02 -0600

    :bug: change standalone for basic deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	output: "standalone",
-	basePath: "",
-  assetPrefix: "",
 	experimental: {
 		serverActions: {
 			bodySizeLimit: "50mb",
